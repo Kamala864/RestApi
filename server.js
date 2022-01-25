17b05ae44b31cea36b9f1bc27d8125dde1c56ab5
Kamala864 <56905936+Kamala864@users.noreply.github.com>
2022-01-25 15:14:38 +0545

    changes on server.js

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
-import express from  'express';
-import { APP_PORT } from './config';
+const express =require("express");
+const morgan=require("morgan");
+const cors=require("cors");
 
 const app= express()
+require('dotenv').config({
+    path:'./config/index.env'
+})
+
+app.use(express.urlencoded({extended:true}));
+app.use(morgan('dev'));
+app.use(cors());
 
 
+app.get('/',(req,res)=>{
+    res.send("test route => home page");
+})
 
 
+app.use((req,res)=>{
+    res.status(404).json({
+        msg:'page not founded'})
+})
 
-app.listen(APP_PORT, ()=> console.log('Listening on port ${APP_PORT}'));
+const PORT=process.env.PORT
 
+app.listen(PORT, ()=>{
+    console.log(`App listining on port ${PORT}`)
+})
